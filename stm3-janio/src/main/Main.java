bfa214249f762d1f38ec16076982f3769a831aa5
janiotobar <janio1992@gmail.com>
2014-11-18 08:22:07 -0500

    Comit inicial del proyecto smt3-janio

---
+++ stm3-janio/src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Proyecto original stm3");
+
+	}
+
+}
