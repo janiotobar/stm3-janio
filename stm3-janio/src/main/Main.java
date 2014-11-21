e4840ddb39e0d3de39c2c47ea35a4a0a597c2f84
janiotobar <janio1992@gmail.com>
2014-11-21 15:16:36 -0500

    Quinto cambio en rama master

--- stm3-janio/src/main/Main.java
+++ stm3-janio/src/main/Main.java
@@ -13,6 +13,7 @@ public class Main {
 		System.out.println("Segundo cambio en rama master");
 		System.out.println("Tercer cambio en rama master");
 		System.out.println("Cuarto cambio en rama master");
+		System.out.println("Quinto cambio en rama master");
 		
 		
 
